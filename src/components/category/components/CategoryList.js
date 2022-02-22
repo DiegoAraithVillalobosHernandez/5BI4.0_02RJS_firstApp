807847e7290b2e9e199807120e3cb02e84a374b8
DiegoAraithVillalobosHernandez <20203tn063@utez.edu.mx>
2022-02-22 14:55:46 -0600

    2

--- src/components/category/components/CategoryList.js
+++ src/components/category/components/CategoryList.js
@@ -1,46 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import { Badge, Col, Row } from 'react-bootstrap';
-import axios from '../../../shared/plugins/axios';
-import DataTable from 'react-data-table-component';
+import React, { useEffect, useState } from "react";
+import { Badge, Button, Card, Col, Row } from "react-bootstrap";
+import axios from "../../../shared/plugins/axios";
+import DataTable from "react-data-table-component";
+import FeatherIcon from 'feather-icons-react';
+import { CategoryForm } from "./CategoryForm";
 
 export const CategoryList = () => {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [categories, setCategories] = useState([]);
+  const [isOpen, setIsOpen] = useState(false)
 
-  const getCategories = () =>{
-    axios({url:"/category/", method:"GET"})
-    .then((response) => {
-      setCategories(response.data);
-      setIsLoading(false)
-    })
-    .catch((error) => {
-      console.log(error);
-    })
+  const getCategories = () => {
+    axios({ url: "/category/", method: "GET" })
+      .then((response) => {
+        setCategories(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-    useEffect(() => {
-       setIsLoading(true);
-       getCategories();
-    }, []);
-    
+  useEffect(() => {
+    setIsLoading(true);
+    getCategories();
+  }, []);
 
-    const columns = [
-      {
-        name: "Categoría",
-        cell: (row) => <div>{row.description}</div>
-      },
-      {
-        name: "Estado",
-        cell: row => row.status.description === "Activo" ?
-              (<Badge pill bg="success">{row.status.description}</Badge> ):(<Badge pill bg='danger'>{row.status.description}</Badge>)
-      }
-    ]
+  const columns = [
+    {
+      name:"#",
+      cell: (row, index) => <div>{index +1}</div>,
+    }
+    ,
+    {
+      name: "Categoría",
+      cell: (row) => <div>{row.description}</div>,
+    },
+    {
+      name: "Estado",
+      cell: (row) =>
+        row.status.description === "Activo" ? (
+          <Badge pill bg="success">
+            {row.status.description}
+          </Badge>
+        ) : (
+          <Badge pill bg="danger">
+            {row.status.description}
+          </Badge>
+        ),
+    },
+  ];
+
+  const paginationOptions = {
+    rowsPerPageText: "Filas por página",
+    rangeSeparatorText: "de",
+  };
 
   return (
-    <Row>
+    <Row className="mt-5">
       <Col>
-        <DataTable columns={columns} data={categories}/>
+        <Card>
+          <Card.Header>
+            <Row>
+              <Col>Categorías</Col>
+              <Col className="text-end">
+              <CategoryForm 
+                isOpen={isOpen} 
+                handleClose={ () => setIsOpen(false)}/>
+                <Button 
+                  variant="success"
+                  onClick={ () => setIsOpen(true)}>
+                  <FeatherIcon icon={"plus"}/>
+                </Button>
+              </Col>
+            </Row>
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>Listado</Card.Title>
+            <Card.Text>
+              <DataTable
+                columns={columns}
+                data={categories}
+                pagination
+                paginationComponentOptions={paginationOptions}
+              />
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </Col>
     </Row>
-  )
-}
+  );
+};
